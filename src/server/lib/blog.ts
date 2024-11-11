0ef3b6794dfafdaf92fd7bdc5d13154071a19d4f
Yannik Knops <yannik.knops@googlemail.com>
2024-11-11 20:14:06 +0100

    Update blog.ts

---
+++ src/server/lib/blog.ts
@@ -0,0 +1,18 @@
+"use server"
+import { PrismaClient, type Blog } from "@prisma/client"
+
+export async function create(){
+
+}
+
+export async function read(){
+
+}
+
+export async function update(){
+
+}
+
+export async function remove(){
+
+}
