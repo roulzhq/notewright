73f83dceea04cbac936be6986aba8aa6f51afe18
Janis Jansen <oss@janis.me>
2024-11-11 21:16:20 +0100

    apply formatter

--- src/server/lib/blog.ts
+++ src/server/lib/blog.ts
@@ -1,30 +1,30 @@
-"use server"
-import { PrismaClient, type Prisma, type Blog, type User, type UserBlogRole } from "@prisma/client"
-import { createUserBlogRole } from "./userBlogRole"
+'use server';
+import {
+  PrismaClient,
+  type Prisma,
+  type Blog,
+  type User,
+  type UserBlogRole,
+} from '@prisma/client';
+import { createUserBlogRole } from './userBlogRole';
 
 const prisma = new PrismaClient();
 
-export async function createBlog(name: string, published: boolean, user: User){
-    const blog: Prisma.BlogCreateInput = {
-        name,
-        published,
-    }
+export async function createBlog(name: string, published: boolean, user: User) {
+  const blog: Prisma.BlogCreateInput = {
+    name,
+    published,
+  };
 
-    const createBlog = await prisma.blog.create({ data: blog });
+  const createBlog = await prisma.blog.create({ data: blog });
 
-    await createUserBlogRole(createBlog, "OWNER", user); 
-    
-    return createBlog.id;
+  await createUserBlogRole(createBlog, 'OWNER', user);
+
+  return createBlog.id;
 }
 
-export async function read(){
+export async function read() {}
 
-}
+export async function update() {}
 
-export async function update(){
-
-}
-
-export async function remove(){
-
-}
+export async function remove() {}
