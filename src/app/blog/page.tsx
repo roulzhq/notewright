fb145de758d61e55576d20091284610d9f015205
Yannik Knops <xelexpro@gmail.com>
2024-11-13 19:40:10 +0100

    Added basic frontend functionality for blog for testing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,18 @@
-export default function Blog() {
-  return <h1>The blog, public stuff</h1>;
+import { getAllPosts } from '../../server/lib/post';
+
+interface BlogProps {
+  blogId: string;
+}
+
+export default async function Blog({ blogId }: BlogProps) {
+  const posts = await getAllPosts(blogId);
+
+  return (
+    <div>
+      <h1>The blog, public stuff</h1>
+      {posts.map(post => (
+        <p key={post.title}>Post: {post.title}</p>
+      ))}
+    </div>
+  );
 }
